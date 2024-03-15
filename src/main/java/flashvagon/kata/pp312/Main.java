c633b67e1d99cfe478a8d456caf0693136090d74
FlashVagON <djvagon@mail.ru>
2024-03-15 19:13:09 +0500

    begin

---
+++ src/main/java/flashvagon/kata/pp312/Main.java
@@ -0,0 +1,17 @@
+package flashvagon.kata.pp312;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class Main {
+
+	public static void main(String[] args)	 {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/flashvagon/kata/pp312/MainTests.java
@@ -0,0 +1,13 @@
+package flashvagon.kata.pp312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
